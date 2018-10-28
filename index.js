683d60487fa9a245bf31556856a281314f397541
GryfHD <44527438+GryfHD@users.noreply.github.com>
2018-10-28 13:51:23 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = "*";
+
+bot.on('ready', function (){
+    bot.user.setActivity("Garder les viewers");
+    console.log(`Connecté sous le nom de Gryf_Bot!`);
+});
+
+bot.login("NTA2MDUyODQyNDcwMTEzMjkx.Drc9zw.Xk_dvTofQztvnDTSuICP7f7wO0I")
+
+
+bot.on('message', message =>{
+    if (message.content === prefix + "help"){
+		Text.channel.send("Liste des commandes: \n -*help \n -*ping");
+		console.log("Et un viewer d'aidé");
+	}
+	
+	if (message.content === prefix + "ping"){;
+		message.reply("pong!");
+		console.log("Ping Pong!");
+	}
+
+	if (message.content === "putain"){;
+		message.delete();
+		message.reply("Oh on se calme !")
+		console.log("Mot vilain maitrisé!")
+	}
+
+	
+});
